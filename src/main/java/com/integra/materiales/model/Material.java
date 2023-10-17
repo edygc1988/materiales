14c9a7f2c485941c3341ecb77d489643e0c6a427
Edison <edygc1988@gmail.com>
2023-10-17 15:08:46 -0500

    New file

--- src/main/java/com/integra/materiales/model/Material.java
+++ src/main/java/com/integra/materiales/model/Material.java
@@ -2,6 +2,8 @@ package com.integra.materiales.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import lombok.Data;
@@ -16,9 +18,14 @@ public class Material {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Name;
-    private String Last_Name;
-    private String Phone;
+    
+    @OneToOne
+    @JoinColumn(name = "id_mov")  // Nombre de la columna de clave foránea en la tabla EgresoMovimiento
+    private EgresoMovimiento egresomovimiento;
+
+    @OneToOne
+    @JoinColumn(name = "numero_pedido")  // Nombre de la columna de clave foránea en la tabla ListasCorte
+    private ListasCorte listascorte;
 
 }
             
