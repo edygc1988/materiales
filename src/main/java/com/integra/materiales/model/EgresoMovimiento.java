93253ff67b67fddc1e2a1121b1a9232ce6b72e54
Edison <edygc1988@gmail.com>
2023-10-17 14:10:18 -0500

    New files

---
+++ src/main/java/com/integra/materiales/model/EgresoMovimiento.java
@@ -0,0 +1,41 @@
+package com.integra.materiales.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EgresoMovimiento {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long id_mov;
+    private String nro_fisico;
+    private String tipo_documento;
+    private Date fecha;
+    private Long id_bodega;
+    private String bodega;
+    private String producto;
+    private Long id_lote;
+    private String identif_bobina;
+    private Long tarjeta;
+    private Long peso;
+    private Long id_proveedor_fk;
+    private String origen;
+    private String division;
+    /* 
+    @ManyToOne
+    @JoinColumn(name = "material_id")  // Nombre de la columna de clave foránea en la tabla Caja
+    private Material material;*/
+}
---
+++ src/main/java/com/integra/materiales/model/ListasCorte.java
@@ -0,0 +1,36 @@
+package com.integra.materiales.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListasCorte {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long numero_pedido;
+    private String cliente;
+    private String simbolo;
+    private String tipo_parte;
+    private Date fecha_ini_produccion;
+    private Long lista_corte;
+    private String division;
+    private String proceso;
+    private Long cantidad_producida_nr;
+    /*
+    @ManyToOne
+    @JoinColumn(name = "material_id")  // Nombre de la columna de clave foránea en la tabla Caja
+    private Material material;*/
+}
